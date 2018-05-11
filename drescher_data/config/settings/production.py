f45008d8de5409cc220c7dc36b2d78c725c46deb
Willem Drescher <willemdrescher@yahoo.com>
2018-05-11 12:17:06 -0400

    www fix?

--- drescher_data/config/settings/production.py
+++ drescher_data/config/settings/production.py
@@ -8,7 +8,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['drescherdata.com'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', 'www.drescherdata.com' default=['drescherdata.com'])
 
 # DATABASES
 # ------------------------------------------------------------------------------
