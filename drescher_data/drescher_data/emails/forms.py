b74438e110b2be8b36ee64ffd4f9c989c41a4408
Willem Drescher <willemdrescher@yahoo.com>
2018-05-17 08:35:15 -0400

    Subscribe

--- drescher_data/drescher_data/emails/forms.py
+++ drescher_data/drescher_data/emails/forms.py
@@ -5,6 +5,7 @@ class ContactForm(forms.ModelForm):
     name = forms.CharField()
     email = forms.EmailField()
     body = forms.CharField(widget = forms.Textarea)
+    subscribe = forms.BooleanField()
     class Meta:
         model =  Contact
         exclude = []
--- drescher_data/drescher_data/emails/models.py
+++ drescher_data/drescher_data/emails/models.py
@@ -5,6 +5,6 @@ class Contact(models.Model):
     name = models.CharField(max_length = 100)
     email = models.CharField(max_length = 200)
     body = models.CharField(max_length = 500)
-
+    subscribe = models.BooleanField()
     def __str__(self):
         return self.email
